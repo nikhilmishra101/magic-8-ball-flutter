a61e9501533cf271065c68eccdef9a3a157ddc21
IEIrodov <nikhilindia30111999@gmail.com>
2020-03-24 19:14:50 +0530

    Second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,55 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+import 'package:audioplayers/audio_cache.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue.shade900,
+        appBar: AppBar(
+          title: Text('Ask Me Anything'),
+          backgroundColor: Colors.blue,
+        ),
+        body: AMA(),
       ),
+    ),
+  );
+}
+
+class AMA extends StatefulWidget {
+  @override
+  _AMAState createState() => _AMAState();
+}
+
+class _AMAState extends State<AMA> {
+  void play() {
+    final player = AudioCache();
+    player.play('note3.wav');
+  }
+
+  int number = 1;
+
+  void Answer() {
+    setState(() {
+      number = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: FlatButton(
+            onPressed: () {
+              Answer();
+              play();
+            },
+            child: Image.asset('images/ball$number.png'),
+          ),
+        ),
+      ],
     );
+  }
+}
